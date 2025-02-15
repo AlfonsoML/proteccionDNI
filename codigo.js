6f20228c7e4fd6ab7e0b54fa62df458ab369a1c2
Alfonso Martinez de Lizarrondo <amla70@gmail.com>
2025-02-15 17:58:02 +0100

    Mover formatos de DNIs a fichero con toda la configuración. Usar UTF8+BOM. Dar formato

--- codigo.js
+++ codigo.js
@@ -1,4 +1,4 @@
-'use strict';
+Ôªø'use strict';
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
@@ -14,32 +14,42 @@ const Horizontal = document.getElementById('Horizontal');
 const Vertical = document.getElementById('Vertical');
 const Zoom = document.getElementById('Zoom');
 
-SelectorFichero.addEventListener('change', function(e) {
+// Rellenar la lista de formatos de DNI autom√°ticamente
+const opciones = []; 
+for (const [key, value] of Object.entries(FormatosDnis)) {
+	opciones.push(`<option value='${key}'>${value.Nombre}</option>`);
+}
+Formato.innerHTML = opciones.join('');
+
+// asignar escucha de eventos
+SelectorFichero.addEventListener('change', function (e) {
 	const fichero = e.target.files[0];
 	if (fichero) {
 		MostrarImagen(fichero);
 	}
 });
 
-// botÛn "bonito" para el usuario
+// bot√≥n "bonito" para el usuario
 document.getElementById('ElegirFoto')
 	.addEventListener('click', () => SelectorFichero.click());
 
 const controles = [Formato, Watermark, Rotacion, Horizontal, Vertical, Zoom];
 
-controles.forEach(function(control) {
-	control.addEventListener('change', function(e) {
-		RedibujarComposicion() ;
+controles.forEach(function (control) {
+	control.addEventListener('change', function (e) {
+		RedibujarComposicion();
 	});
 });
 
+// Al hacer click en la imagen guardarla ¬øes intuitivo? ¬øes mejor un bot√≥n separado?
 canvas.addEventListener('click', GrabarImagen);
 
 /**
- Dibujar cargar la imagen en img, redimensionar el canvas para que sea proporcional y comenzar proceso
-*/
+ * Dibujar cargar la imagen en img, redimensionar el canvas para que sea proporcional y comenzar proceso
+ * @param {any} file
+ */
 function MostrarImagen(file) {
-	img.onload = function() {
+	img.onload = function () {
 		URL.revokeObjectURL(img.src)
 
 		const ancho = img.width;
@@ -47,13 +57,13 @@ function MostrarImagen(file) {
 		const ratio = ancho / alto;
 		canvas.height = canvas.width * alto / ancho;
 
-		RedibujarComposicion() ;
+		RedibujarComposicion();
 	}
 	img.src = URL.createObjectURL(file);
 }
 
 /**
-Proceso de protecciÛn del DNI
+Proceso de protecci√≥n del DNI
 */
 function RedibujarComposicion() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -74,9 +84,9 @@ function RedibujarComposicion() {
 
 		// draw the image
 		// since the context is rotated, the image will be rotated also
-		ctx.drawImage(img, - (canvas.width / 2),  - (canvas.height / 2), canvas.width * Zoom.value, canvas.height * Zoom.value);
+		ctx.drawImage(img, - (canvas.width / 2), - (canvas.height / 2), canvas.width * Zoom.value, canvas.height * Zoom.value);
 
-		// weíre done with the rotating so restore the unrotated context
+		// we‚Äôre done with the rotating so restore the unrotated context
 		ctx.restore();
 	}
 
@@ -84,30 +94,12 @@ function RedibujarComposicion() {
 
 	DibujarMarcaAgua();
 
-	ConvertirBN(); 
+	ConvertirBN();
 }
 
-/** Ocultar las partes de la imagen que no hacen ninguna falta, depender· del formato de DNI y el lado */
+/** Ocultar las partes de la imagen que no hacen ninguna falta, depender√° del formato de DNI y el lado */
 function DibujarMascara() {
-const todosBloques = { 
-		'DNI1-Frontal': [
-			{x: 750, y: 80, w: 140, h: 60},
-			{x: 380, y: 380, w: 240, h: 35}, // Fecha Nacimiento
-			{x: 800, y: 370, w: 140, h: 40},
-			{x: 380, y: 445, w: 400, h: 35}, // Num Soporte, Validez
-			{x: 380, y: 500, w: 400, h: 100}, // Firma
-			{x: 790, y: 525, w: 210, h: 75},
-		],
-		'DNI1-Trasera': [
-			{x: 100, y: 100, w: 120, h: 40},
-			{x: 0, y: 190, w: 60, h: 180}, // vertical
-			{x: 240, y: 210, w: 500, h: 80}, // Nacimiento
-			{x: 240, y: 340, w: 500, h: 40}, // Padres
-			{x: 20, y: 410, w: 960, h: 130}, // Inferior
-		]
-	}
-
-	const bloques = todosBloques[Formato.value];
+	const bloques = FormatosDnis[Formato.value].Mascaras;
 
 	ctx.fillStyle = 'black';
 	bloques.forEach(bloque => ctx.fillRect(bloque.x, bloque.y, bloque.w, bloque.h));
@@ -116,26 +108,7 @@ const todosBloques = {
 function DibujarMarcaAgua() {
 	const texto = Watermark.value;
 
-	const todasMarcas = {
-		'DNI1-Frontal': [{
-			fuente: '900 48px sans-serif',
-			estilo: 'rgb(0 0 0 / 100%)',
-			bb: {x: 10, y: 140, w:350, h: 460}
-		}, {
-			fuente: '20px serif',
-			estilo: 'rgb(0 0 0 / 30%)',
-			bb: {x: 10, y: 70, w: 980, h: 60}
-		}
-		],
-		'DNI1-Trasera': [{
-			fuente: '12px serif',
-			estilo: 'rgb(0 0 0 / 30%)',
-			bb: {x: 10, y: 70, w: 980, h: 100}
-		}
-		],
-	};
-
-	const marcas = todasMarcas[Formato.value];
+	const marcas = FormatosDnis[Formato.value].Watermarks;
 	marcas.forEach(marca => {
 		ctx.font = marca.fuente;
 		ctx.fillStyle = marca.estilo;
@@ -144,7 +117,14 @@ function DibujarMarcaAgua() {
 
 }
 
-/** Escribir un texto en la zona delimitada haciendo wrap letra a letra y repitiendo hasta llenar */
+/**
+ * Escribir un texto en la zona delimitada haciendo wrap letra a letra y repitiendo hasta llenar
+ * @param {any} texto
+ * @param {any} x
+ * @param {any} y
+ * @param {any} maxWidth
+ * @param {any} maxHeight
+ */
 function RellenarTexto(texto, x, y, maxWidth, maxHeight) {
 	// Calcular altura de linea con la fuente actual
 	const metrics = ctx.measureText("A");
@@ -152,56 +132,62 @@ function RellenarTexto(texto, x, y, maxWidth, maxHeight) {
 	const yMax = y + maxHeight - lineHeight;
 
 	// Dividir el texto en letras (con un separador final para repeticiones)
-    const letras = (texto + ' - ').split('');
-    let line = ''; // Linea que vamos a escribir
+	const letras = (texto + ' - ').split('');
+	let line = ''; // Linea que vamos a escribir
 
-    // bucle hasta rellenar toda la zona, vamos letra a letra
+	// bucle hasta rellenar toda la zona, vamos letra a letra
 	let n = 0;
-    while (true) {
+	while (true) {
 		const letra = letras[n];
-        // Medir la anchura que tendr· si aÒadimos esta letra
-        const testWidth = ctx.measureText(line + letra).width;
-        // If the width of this test line is more than the max width
-        if (testWidth > maxWidth && n > 0) {
+		// Medir la anchura que tendr√° si a√±adimos esta letra
+		const testWidth = ctx.measureText(line + letra).width;
+		// If the width of this test line is more than the max width
+		if (testWidth > maxWidth && n > 0) {
 			// escribir el texto
-            ctx.fillText(line, x, y);
-            // Nos movemos abajo seg˙n la altura calculada
-            y += lineHeight;
-			// cuando superemos el lÌmite vertical paramos
+			ctx.fillText(line, x, y);
+			// Nos movemos abajo seg√∫n la altura calculada
+			y += lineHeight;
+			// cuando superemos el l√≠mite vertical paramos
 			if (y >= yMax)
 				return;
 
-            // Comenzamos una linea nueva con esta letra
-            line = letra;
-        } else {
-            // Si no hemos superado la anchura, la aÒadimos a la linea actual
-            line += letra;
-        }
-        // cuando llegamos al final, reseteamos para volver
-        if (n === letras.length - 1) 
-             n = 0;
+			// Comenzamos una linea nueva con esta letra
+			line = letra;
+		} else {
+			// Si no hemos superado la anchura, la a√±adimos a la linea actual
+			line += letra;
+		}
+		// cuando llegamos al final, reseteamos para volver
+		if (n === letras.length - 1)
+			n = 0;
 		else
 			n++;
-    }
+	}
 }
 
 
-/** Convertir todo a blanco y negro */
+/**
+ * Convertir todo a blanco y negro
+ */
 function ConvertirBN() {
 	const w = canvas.width;
 	const h = canvas.height;
 
-    const imgPixels = ctx.getImageData(0, 0, w, h);
-    for (let y = 0; y < imgPixels.height; y++) {
-		for (let x = 0; x < imgPixels.width; x++) {
-			const i = (y * 4) * imgPixels.width + x * 4;
-			const avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
-			imgPixels.data[i] = avg;
-			imgPixels.data[i + 1] = avg;
-			imgPixels.data[i + 2] = avg;
+	try {
+		const imgPixels = ctx.getImageData(0, 0, w, h);
+		for (let y = 0; y < imgPixels.height; y++) {
+			for (let x = 0; x < imgPixels.width; x++) {
+				const i = (y * 4) * imgPixels.width + x * 4;
+				const avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+				imgPixels.data[i] = avg;
+				imgPixels.data[i + 1] = avg;
+				imgPixels.data[i + 2] = avg;
+			}
 		}
+		ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+	} catch (e) {
+		// da error al usar imagen de prueba
 	}
-	ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
 }
 
 function GrabarImagen() {
---
+++ formatos.js
@@ -0,0 +1,40 @@
+﻿// Conjunto con objetos que definen los datos para cada formato de DNI
+// .Mascaras: los rectángulos a ocultar con negro
+// .Watermarks: array de objetos que definen fuente, estilo y rectángulo a rellenar con el watermark elegido
+const FormatosDnis = {
+	'DNI1-Frontal': {
+		Nombre: 'DNI1 - Frontal',
+		Mascaras: [
+			{ x: 750, y: 80, w: 140, h: 60 },
+			{ x: 380, y: 380, w: 240, h: 35 }, // Fecha Nacimiento
+			{ x: 800, y: 370, w: 140, h: 40 },
+			{ x: 380, y: 445, w: 400, h: 35 }, // Num Soporte, Validez
+			{ x: 380, y: 500, w: 400, h: 100 }, // Firma
+			{ x: 790, y: 525, w: 210, h: 75 },
+		],
+		Watermarks: [{
+			fuente: '900 48px sans-serif',
+			estilo: 'rgb(0 0 0 / 100%)',
+			bb: { x: 10, y: 140, w: 350, h: 460 }
+		}, {
+			fuente: '20px serif',
+			estilo: 'rgb(0 0 0 / 30%)',
+			bb: { x: 10, y: 70, w: 980, h: 60 }
+		}]
+	},
+	'DNI1-Trasera': {
+		Nombre: 'DNI1 - Trasera',
+		Mascaras: [
+			{ x: 100, y: 100, w: 120, h: 40 },
+			{ x: 0, y: 190, w: 60, h: 180 }, // vertical
+			{ x: 240, y: 210, w: 500, h: 80 }, // Nacimiento
+			{ x: 240, y: 340, w: 500, h: 40 }, // Padres
+			{ x: 20, y: 410, w: 960, h: 130 }, // Inferior
+		],
+		Watermarks: [{
+			fuente: '12px serif',
+			estilo: 'rgb(0 0 0 / 30%)',
+			bb: { x: 10, y: 70, w: 980, h: 100 }
+		}]
+	},
+};
