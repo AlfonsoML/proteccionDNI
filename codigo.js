37a78294ad30e4b39187210ee217a5a1cce90780
Alfonso Martinez de Lizarrondo <amla70@gmail.com>
2025-03-25 20:37:56 +0100

    Cambiar la manera de mostrar las anclas en FAQ para que no de problemas de accesibilidad

--- codigo.js
+++ codigo.js
@@ -156,20 +156,21 @@ configurarPantallaCompleta();
 const hash = document.location.hash;
 if (hash) {
 	const info = document.querySelector(hash);
-	if (info && typeof info.open != 'undefined')
+	if (info && typeof info.open != 'undefined') {
 		info.open = true;
+		info.firstElementChild.focus();
+	}
 }
 
 // Generar enlaces visibles en los ids
 querySelector_Array('#FAQ details[id]')
 	.forEach(elmto => {
-		const id = elmto.id;
-		const anchor = document.createElement('a');
-		anchor.href = '#' + id;
-		anchor.innerHTML = '<svg viewBox="0 0 24 24" role="presentation"><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"/></svg>';
-		anchor.className = 'ancla';
-		anchor.onclick = function () { elmto.open = true };
-		elmto.firstElementChild.appendChild(anchor);
+		elmto.addEventListener('click', (e) => {
+			if (elmto.open)
+				history.replaceState(null, '', ' ')
+			else
+				history.replaceState(null, '', '#' + elmto.id)
+		});
 	});
 
 // Si el navegador soporta el Popover API, mostraremos la información de ayuda como tooltips sin salir del modo edición
